69f8000d328f6e3d2a3905fa967ca3b59c7bd42f
Evelin Cobos <evecobos8@gmail.com>
2025-10-17 23:55:51 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 //write your code here
+
+const countLetterFrequency = (sentence) => {
+    let obj = {}
+
+
+    for (let i = 0; i < sentence.length; i++) {
+        if (sentence[i] === ' ') {
+            continue
+            //guard clause for space so it doesn't run the code below and goes to next character 
+        }
+
+        if (sentence[i] in obj) {
+            obj[sentence[i]]++
+            //console.log(obj)
+            //check for repeat of key then add one to value if true
+
+        } else {
+            obj[sentence[i]] = 1
+
+        }
+    }
+    return obj
+
+}
+console.log(countLetterFrequency('cat and the hat'))
